edbc97c83b54d53b2b8c4ec613d4d733f19c1d1b
Gonzalo Palacios <44221280+chalo95@users.noreply.github.com>
2021-10-24 20:54:17 -0600

    incorporacion de user y password

--- proyecto_too/proyecto_too/Models/bdtooContext.cs
+++ proyecto_too/proyecto_too/Models/bdtooContext.cs
@@ -39,6 +39,7 @@ namespace proyecto_too.Models
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("server=DESKTOP-9DSSP5T\\SQLEXPRESS; Database=bdtoo; Trusted_Connection=True;");
+                //optionsBuilder.UseSqlServer("server=DESKTOP-9DSSP5T\\SQLEXPRESS; Database=bdtoo;User ID=sa; Password=Admin123$");
             }
         }
 
