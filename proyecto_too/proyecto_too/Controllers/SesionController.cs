1d34493968e75b7660bf04160214d77153d2b2f8
Gonzalo Palacios <44221280+chalo95@users.noreply.github.com>
2021-10-26 18:20:38 -0600

    login html

--- proyecto_too/proyecto_too/Controllers/SesionController.cs
+++ proyecto_too/proyecto_too/Controllers/SesionController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace proyecto_too.Controllers
 {
@@ -84,4 +86,6 @@ namespace proyecto_too.Controllers
             }
         }
     }
+
 }
+
